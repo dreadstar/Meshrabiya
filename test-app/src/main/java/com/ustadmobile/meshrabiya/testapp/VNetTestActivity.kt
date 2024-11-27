31ccc29d63a2798759969027f688fca62a349675
Mike Dawson <mike@ustadmobile.com>
2024-11-27 12:40:54 +0400

    Periodically ping all known neighbors to track ping times.
    Remove obsolete 'this node' screen

--- test-app/src/main/java/com/ustadmobile/meshrabiya/testapp/VNetTestActivity.kt
+++ test-app/src/main/java/com/ustadmobile/meshrabiya/testapp/VNetTestActivity.kt
@@ -2,7 +2,6 @@ package com.ustadmobile.meshrabiya.testapp
 
 import android.Manifest
 import android.content.Intent
-import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
@@ -10,16 +9,14 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Chat
+import androidx.compose.material.icons.automirrored.filled.Chat
 import androidx.compose.material.icons.filled.ConnectWithoutContact
 import androidx.compose.material.icons.filled.Download
 import androidx.compose.material.icons.filled.Info
-import androidx.compose.material.icons.filled.PhoneAndroid
 import androidx.compose.material.icons.filled.UploadFile
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExtendedFloatingActionButton
@@ -40,7 +37,6 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -51,14 +47,12 @@ import com.ustadmobile.meshrabiya.testapp.domain.AddNearbyNetworkUseCase
 import com.ustadmobile.meshrabiya.testapp.screens.InfoScreen
 import com.ustadmobile.meshrabiya.testapp.screens.LogListScreen
 import com.ustadmobile.meshrabiya.testapp.screens.NearbyTestRoute
-import com.ustadmobile.meshrabiya.testapp.screens.NearbyTestScreen
 import com.ustadmobile.meshrabiya.testapp.screens.NeighborNodeListScreen
 import com.ustadmobile.meshrabiya.testapp.screens.OpenSourceLicensesScreen
 import com.ustadmobile.meshrabiya.testapp.screens.ReceiveScreen
 import com.ustadmobile.meshrabiya.testapp.screens.SelectDestNodeScreen
 import com.ustadmobile.meshrabiya.testapp.screens.SendFileScreen
 import com.ustadmobile.meshrabiya.testapp.theme.HttpOverBluetoothTheme
-import com.ustadmobile.meshrabiya.testapp.viewmodel.NearbyTestViewModel
 import com.ustadmobile.meshrabiya.testapp.viewmodel.VpnTestViewModel
 import org.kodein.di.DI
 import org.kodein.di.DIAware
@@ -81,13 +75,13 @@ class VNetTestActivity : ComponentActivity(), DIAware {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-//                    VpnTestScreen(viewModel = viewModel, onStartVpn = { startVpn() })
                     MeshrabiyaTestApp(di = di)
                 }
             }
         }
     }
 
+    @Suppress("unused")
     private fun startVpn() {
         val intent = viewModel.prepareVpn()
         if (intent != null) {
@@ -206,7 +200,7 @@ fun MeshrabiyaTestApp(
                     },
                     icon = {
                         Icon(
-                            imageVector = Icons.Default.Chat,
+                            imageVector = Icons.AutoMirrored.Filled.Chat,
                             contentDescription = null,
                         )
                     }
@@ -278,7 +272,7 @@ fun MeshrabiyaTestApp(
 fun AppNavHost(
     modifier: Modifier = Modifier,
     navController: NavHostController = rememberNavController(),
-    startDestination: String = "localvirtualnode",
+    startDestination: String = "neighbornodes",
     onSetAppUiState: (AppUiState) -> Unit = { },
     snackbarHostState: SnackbarHostState,
 ) {
